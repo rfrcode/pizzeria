c3c82474ebf5fbc58cf78b2ef738161a1a2d7a6e
rfrcode <60968161+rfrcode@users.noreply.github.com>
2020-02-26 01:08:57 +0100

    +pizzeria again

---
+++ src/pizzeria/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using pizzeria.dtos;
+using pizzeria.services;
+
+namespace pizzeria.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserService _userService;
+
+        public UserController(ILogger<UserController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+            ///_userService = new ServiceUsers(); SE VIOLA LA D DE SOLID
+            // ServiceDescriptor  para registro de dependencias
+
+        }
+
+
+               //   //actionresult
+            //task http response  
+            //async //await
+            //http response
+            // cambiar void para retornar algo
+            // si es valido retorna token
+
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody]DTORegister userRegister)
+        { 
+            var result = _userService.Register(userRegister);
+            return Ok(result);
+           
+        }
+    }
+}

981352d377293957d00ec3d165ccbbaa86b2fdf1
opinno <sfalla87@hotmail.com>
2020-03-03 12:38:16 +0100

    fix ingredient create

--- src/pizzeriadomain/Ingredient.cs
+++ src/pizzeriadomain/Ingredient.cs
@@ -1,13 +1,24 @@
 using System.ComponentModel.DataAnnotations;
 using System;
 using pizzeria.dtos;
+
 namespace pizzeria.Domain
 {
     public class Ingredient{
         public Guid id {get; set;}
         public string Name {get; set; }
         public Decimal price {get; set; }
-         
-
+        
+        public static Ingredient Create(DTOIngredient newPizza)
+        {
+            var ingredient = new Ingredient();     
+            ingredient.id= Guid.NewGuid();
+            ingredient.Name= newPizza.Name;
+            ingredient.price = newPizza.price;
+            
+            return ingredient;
+        }
     }
+
+    
 }
